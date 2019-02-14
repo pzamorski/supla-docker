27c1e7d2961f3c2942293c5b8cd5dd4f2029ebdb
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2019-02-14 14:15:40 +0100

    cd proj

--- supla-core/run.sh
+++ supla-core/run.sh
@@ -2,5 +2,6 @@
 
 PROJ=~/CProjects
 [ -e $PROJ ] || mkdir -p $PROJ
+cd "$PROJ"
 [ ! -e ./supla-core ] && git clone https://github.com/SUPLA/supla-core
 docker run -v "$PROJ":/CProjects -it devel/supla-core /bin/bash
