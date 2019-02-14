ff61695393a850dceabe02f187f510a17b89040e
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2019-02-14 12:43:31 +0100

    Initial commit

---
+++ supla-core/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+PROJ=~/CProjects
+[ -e $PROJ ] || mkdir -p $PROJ
+[ ! -e ./supla-core ] && git clone https://github.com/SUPLA/supla-core
+docker run -v "$PROJ":/CProjects -it devel/supla-core /bin/bash
